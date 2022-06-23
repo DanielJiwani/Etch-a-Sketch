9d8bc00c3f6999e67d49048e596d066f3187b720
Daniel <daniel_jiwani05@hotmail.com>
2022-06-23 18:30:44 -0500

    added color fill

--- javascript.js
+++ javascript.js
@@ -1,10 +1,43 @@
-const container = document.getElementById("container");
+//Sets important constants and variables
 
-function defaultGrid() {
-    for(let i = 0; i < 256; i++) {
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "cell";
-    }
-}
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
 
 defaultGrid();
+
+
+//Creates a default grid sized 16x16 
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+    
+}
+
+//Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+
+    //Creates rows
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+
+//Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
+
+container.addEventListener('mouseover', function(e) {
+    if(e.target.matches(".cell")) {
+        e.target.classList.add("active");
+    }
+})
